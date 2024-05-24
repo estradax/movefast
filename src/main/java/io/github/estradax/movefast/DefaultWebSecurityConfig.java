440ee166b4fa6ca6b334d42d1272a8d64d4d14a1
estradax <azharumuhammad@gmail.com>
2024-05-25 00:36:16 +0700

    feat: add logout logic

--- src/main/java/io/github/estradax/movefast/DefaultWebSecurityConfig.java
+++ src/main/java/io/github/estradax/movefast/DefaultWebSecurityConfig.java
@@ -16,11 +16,22 @@ public class DefaultWebSecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests((requests) -> requests
-          .requestMatchers("/register", "/setauth").permitAll()
+          .requestMatchers(
+              "/",
+              "/register",
+              "/assets/**",
+              "/ncss/**",
+              "/nfonts/**",
+              "/nimages/**",
+              "/njs/**"
+          ).permitAll()
           .anyRequest().authenticated()
         )
         .formLogin((form) -> form
             .loginPage("/login")
+            .permitAll()
+        )
+        .logout((logout) -> logout
             .permitAll()
         );
 
